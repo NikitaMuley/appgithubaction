fb8594b04918151d0f21a697bf8e4e1fb353058e
NikitaMuley <150451478+NikitaMuley@users.noreply.github.com>
2025-04-06 00:54:50 +0530

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,7 +1,7 @@
-from src.math_operations import add,sub
+from src.math_operations import add,sub  # importing add, sub functions fom math_operations.py file located in src folder
 
 def test_add():
-    assert add(2,3)==5
+    assert add(2,3)==5   # assert checks if the statement is True
     assert add(-1,1)==0
     
 def test_sub():
